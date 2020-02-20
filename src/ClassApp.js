b802b55d66b1cf381edac69afc41058b7aa62b3a
Manish Tripathy <manisht@thoughtworks.com>
2020-02-20 06:46:43 +0530

    Preferred way of outputting conditional content

--- src/ClassApp.js
+++ src/ClassApp.js
@@ -36,30 +36,33 @@ class ClassApp extends Component {
         })
     }
 
-
     render() {
+        let personsDiv = null
+        if (this.state.showPersons) {
+            personsDiv = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                        click={this.switchNameHandler.bind(this, "Haladi")}
+                    />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={() => this.switchNameHandler("Basanta")}
+                        changed={this.nameChangeHandler}
+                    />
+                    {/*The above can be a convenient syntax but is not recommended
+                    as react might end up re-rendering a log. TODO: Google this to find more*/}
+
+                    <button onClick={this.switchNameHandler.bind(this, "Sasur")}>Change Names</button>
+                </div>
+            )
+        }
         return (
             <div>
                 <button onClick={this.togglePesonHandler}>Show/Hide Persons</button>
-                {this.state.showPersons ?
-                    <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age}
-                            click={this.switchNameHandler.bind(this, "Haladi")}
-                        />
-                        <Person
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={() => this.switchNameHandler("Basanta")}
-                            changed={this.nameChangeHandler}
-                        />
-                        {/*The above can be a convenient syntax but is not recommended
-                    as react might end up re-rendering a log. TODO: Google this to find more*/}
-
-                        <button onClick={this.switchNameHandler.bind(this, "Sasur")}>Change Names</button>
-                    </div> : null}
-
+                {personsDiv}
             </div>
         )
     }
